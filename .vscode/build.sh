5693dd6c6c5d136db7a456aa739b5e838a31a179
Sfehhrths <1615845+Sfehhrths@users.noreply.github.com>
2025-04-06 17:58:36 +0900

    fix

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p iOS/usr/bin
 cp azule iOS/usr/bin/azule
+mkdir -p iOS/usr/lib/Azule/modules
 cp modules/azule_apt iOS/usr/lib/Azule/modules/
 cp modules/azule_decrypt iOS/usr/lib/Azule/modules/
 find iOS -name .DS_Store -exec rm -rf {} \;
-dpkg-deb -b iOS Azule-iOS.deb
-rm iOS/usr/bin/azule iOS/usr/lib/Azule/modules/azule_decrypt iOS/usr/lib/Azule/modules/azule_apt 
+dpkg-deb -Zgzip -b iOS Azule-iOS.deb
+rm iOS/usr/bin/azule iOS/usr/lib/Azule/modules/azule_decrypt iOS/usr/lib/Azule/modules/azule_apt
